6c305581fa515e93cfbe862c03d340d1b0ca9cb8
Hillary Gospodarek <gospodarekh@gmail.com>
2016-08-07 22:36:00 -0400

    Add tests for Sign up, in, out, and settings

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:email) { |n| "person#{n}@example.com" }
+    sequence(:username) { |n| "person#{n}" }
+    first_name "Jon"
+    last_name "Smith"
+    password "password"
+    password_confirmation "password"
   end
 end
